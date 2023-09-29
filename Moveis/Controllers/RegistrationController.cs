d3d7f4cc6b80dbf8ca3ddb68a91d2df88f3a189b
admin <admin@DESKTOP-HASCPO2>
2023-09-29 17:24:47 +0400

    New commit 17:24

---
+++ Moveis/Controllers/RegistrationController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Moveis.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult RegistrationForm()
+        {
+            return View();
+        }
+    }
+}
