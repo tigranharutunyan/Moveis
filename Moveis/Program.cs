be9519586fb80c6d43d9fdc42de7f9ba558e6da9
Tigran <134860695+tigranharutunyan@users.noreply.github.com>
2023-09-29 17:26:42 +0400

    Changes

--- Moveis/Program.cs
+++ Moveis/Program.cs
@@ -1,4 +1,8 @@
 using Microsoft.EntityFrameworkCore;
+using Moveis.Data.Repositories;
+using Moveis.Data.Repositories.Interface;
+using Moveis.Service.Interface;
+using Moveis.Service;
 using Moveis.Data;
 
 namespace Moveis
@@ -13,8 +17,18 @@ namespace Moveis
             builder.Services.AddControllersWithViews();
             builder.Services.AddDbContext<MyFilmContext>(
                 option => option.UseSqlServer(builder.Configuration.GetConnectionString("MyFilmMV50")));
+           
+            builder.Services.AddScoped<IFilmRepostory, FilmRepository>();
+            builder.Services.AddScoped<IDirectorRepository, DirectorRepository>();
+            builder.Services.AddScoped<ICountryRepository, CountryRepository>();
+            builder.Services.AddScoped<IAdminRepository, AdminRepository>();
+            builder.Services.AddScoped<IUserRepository, UserRepository>();
+            builder.Services.AddScoped<IFilmService, FilmService>();
+            builder.Services.AddScoped<IDirectorService, DirectorService>();
+            builder.Services.AddScoped<ICountryService, CountryService>();
+            builder.Services.AddScoped<IAdminService, AdminService>();
+            builder.Services.AddScoped<IUserService, UserService>();
             var app = builder.Build();
-
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
