fa454e70f17e8dc166dcb5900e5e5f5ed1a759b5
Aren Asulyan <ArenVan@DESKTOP-EVQKGK3>
2023-10-06 18:17:30 +0400

    fix

--- Moveis/Controllers/HomeController.cs
+++ Moveis/Controllers/HomeController.cs
@@ -11,34 +11,22 @@ namespace Moveis.Controllers
 {
     public class HomeController : Controller
     {
-<<<<<<< HEAD
         private readonly IDirectorService _directorService;
         private readonly IFilmService _filmService;
 
-        public HomeController(IDirectorService directorService, IFilmService filmService) { 
-             _directorService = directorService;
+        public HomeController(IDirectorService directorService, IFilmService filmService)
+        {
+            _directorService = directorService;
             _filmService = filmService;
 
-          }
+        }
         [HttpGet]
         public IActionResult Index()
         {
             FilmAddEditDropDown model = new FilmAddEditDropDown();
             ViewBag.Filter = _filmService.GetByFilter(model);
-//=======
-        private readonly IFilmService _filmService;
-        public HomeController(IFilmService filmService)
-        {
-            _filmService = filmService;
-        }
-    
-        public IActionResult Index(FilmAddEdit model)
-        {
-            
-//>>>>>>> 5c49ab8d2bd5b60a4d506c4a6e02533b2987da64
             return View();
-         
-          }
+        }
 
         [HttpPost]
         public IActionResult Index(FilmAddEditDropDown model)
@@ -46,23 +34,17 @@ namespace Moveis.Controllers
             ViewBag.Filter = _filmService.GetByFilter(model);
             return View();
 
-         }
+        }
         public IActionResult UserList()
         {
 
             return View();
         }
-<<<<<<< HEAD
 
-        
-
-       
-=======
         public IActionResult FilmFilter(FilmAddEditDropDown model)
         {
-             ViewBag.film = _filmService.GetByFilter(model);
+            ViewBag.film = _filmService.GetByFilter(model);
             return View();
         }
->>>>>>> 5c49ab8d2bd5b60a4d506c4a6e02533b2987da64
     }
 }
